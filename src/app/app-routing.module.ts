4946a5ea039840f6d1834023044194c86e859185
gonzalo <meru.vi06@gmail.com>
2018-12-20 19:57:21 -0400

    CREATED ROUTES MODULE AND USER COMPONENT

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    { path:'', redirectTo: '/appComponent', pathMatch: 'full'},
+    { path:'appComponent', component: AppComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule { }
